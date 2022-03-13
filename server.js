a62decfa8f15a7bb21989f2020d098a9c3ec9d07
Marcelo Almeida <marcelocralmeida@gmail.com>
2022-03-13 15:24:42 -0300

    Update code to use express for server functionality

--- server.js
+++ server.js
@@ -1,48 +1,11 @@
-const http = require('http');
-const fs = require('fs').promises;
-
-const host = 'localhost';
-const port = 8000;
-
-let files = {};
+const express = require('express')
+const app = express()
+const port = 8000
 
 
-const requestListener = function (req, res) {
-  switch (req.url) {
-    case '/': {
-      res.setHeader('Content-Type', 'text/html');
-      res.writeHead(200);
-      res.end(files.index);
-      break;
-    }
-    case '/transactions': {
-      res.setHeader('Content-Type', 'text/html');
-      res.writeHead(200);
-      res.end(files.transactions);
-      break;
-    }
-    default: {
-      res.setHeader('Content-Type', 'text/html');
-      res.writeHead(404);
-      res.end('Not found');
-    }
-  }
-}
+app.use(express.static('public'))
 
 
-const server = http.createServer(requestListener);
-server.listen(port, host, () => {
-  console.log(`Server is running on http://${host}:${port}`);
-});
-
-
-['index', 'transactions'].map(fileName => {
-  fs.readFile(__dirname + `/${fileName}.html`)
-    .then(contents => {
-      files[fileName] = contents;
-    })
-    .catch(err => {
-      console.error(`Could not read '${fileName}.html' file: ${err}`);
-      process.exit(1);
-    });
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`)
 })
