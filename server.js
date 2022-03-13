bbd57b0e558c164d540dac27972627583e37db74
Marcelo Almeida <marcelocralmeida@gmail.com>
2022-03-13 15:53:20 -0300

    Add multer support, for file upload

--- server.js
+++ server.js
@@ -1,9 +1,18 @@
 const express = require('express')
+const multer = require('multer')
+
+const storage = multer.memoryStorage()
+const upload = multer({ storage: storage })
 const app = express()
 const port = 8000
 
 
 app.use(express.static('public'))
+
+
+app.post('/upload_file', upload.single('uploaded_file'), (req, res) => {
+  console.log(req.file.buffer.toString())
+})
 
 
 app.listen(port, () => {
